bf298ffe101d721ff2ed18f0a882e3a3aefac758
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-08 21:07:44 +0200

    Initial commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Martin Flischman on 2023/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Martin Flischman on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
