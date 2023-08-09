9d170d4b225287288a419f9bd0efe0bfadae49b0
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-09 12:51:35 +0200

    CustomBackground & Utility colors
    feat: LinearGradient from light to medium green added to the CustomBackgroundView, including a cornerRadius of 40.
    
    feat: Padding added to the preview of the CustomBackgroundView.
    
    feat: Utility folder created and a ColorExtension file added. This is to hold all the color extensions for re-use in the app.

--- Hike/Component/CustomBackgroundView.swift
+++ Hike/Component/CustomBackgroundView.swift
@@ -9,12 +9,36 @@ import SwiftUI
 
 struct CustomBackgroundView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            // Mark: - 3. Depth
+            Color("ColorGreenDark")
+                .cornerRadius(40)
+                .offset(y: 12)
+            
+            // Mark: - 2. Light
+            Color("ColorGreenLight")
+                .cornerRadius(40)
+                .offset(y: 3)
+                .opacity(0.85)
+            
+            // Mark: - 1. Surface
+            
+            LinearGradient(
+                colors: [
+                    Color("ColorGreenLight"),
+                    Color("ColorGreenMedium")
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .cornerRadius(40)
+        }
     }
 }
 
 struct CustomBackgroundView_Previews: PreviewProvider {
     static var previews: some View {
         CustomBackgroundView()
-    }
+        .padding()    }
 }
