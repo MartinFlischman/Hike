c0d245e4fc2eec015f32aef382d3e40abaf39afe
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-11 15:31:11 +0200

    Settings Sheet
    feat: ContentView file embedded in a new folder named "Screen".
    
    feat: New SwiftUI file created, "SettingsView" and added to the Screen folder.

---
+++ Hike/Screen/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by Martin Flischman on 2023/08/11.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
