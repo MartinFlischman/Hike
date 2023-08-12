6b5d95f8dce922f736f0fb5a369268d9ce87c10d
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-12 15:33:16 +0200

    Header Section
    feat: List view created that houses the newly created header for the SettingsView.
    
    feat: Header and header content/text section created, styled and aligned.

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -12,11 +12,66 @@ struct SettingsView: View {
         List {
             // Section: Header
             
-            Text("Settings View")
+            Section {
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80, weight: .black))
+                    
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66, weight: .black))
+                        
+                        Text("Editors' Choice")
+                            .fontWeight(.medium)
+                    }
+                    
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80, weight: .black))
+                    
+                    Spacer()
+                }
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [
+                            Color.customGreenLight,
+                            Color.customGreenMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .padding(.top, 8)
+                
+                VStack(spacing: 8) {
+                    Text("Where can you find \nperfect trails?")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    
+                    Text("The hike which looks gorgeous in photos, but is even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
+                        .font(.footnote)
+                        .italic()
+                    
+                    Text("Dust off your boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.customGreenMedium)
+                        .frame(maxWidth: .infinity)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 16)
+
+                
+            } // Header - End
+            .listRowSeparator(.hidden )
+            
             
             // Section: Icons
             
+            
             // Section: About
+            
+            
         } // List - End
     }
 }
