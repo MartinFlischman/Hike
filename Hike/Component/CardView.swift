acff8a1dd8a7ea23494a7d2096a62b06a0d22558
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-13 18:47:46 +0200

    SettingsView
    feat: Added a Medium and Large Detent for the SettingsView page, to the CardView.

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -71,6 +71,7 @@ struct CardView: View {
                         .sheet(isPresented: $isShowingSheet) {
                             SettingsView()
                                 .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     
